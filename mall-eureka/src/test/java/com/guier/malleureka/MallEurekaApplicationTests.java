4252aef2dc8a5983c93a7e38c0a347cf5781fce0
Guiee <466007639@qq.com>
2020-04-25 22:41:51 +0800

    eureka-server

---
+++ mall-eureka/src/test/java/com/guier/malleureka/MallEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guier.malleureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MallEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
